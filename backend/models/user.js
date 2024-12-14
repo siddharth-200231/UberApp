3c9e84211041e744a56e9fa2b1ef59485f6c6a81
Siddharth <sahusidd715@gmail.com>
2024-12-14 20:59:26 +0530

    user model added

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,27 @@
 const mongoose = require ('mongoose')
 const userSchema = mongoose.Schema({
-    fullname
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [ 3, 'First name must be at least 3 characters long' ],
+        },
+        lastname: {
+            type: String,
+            minlength: [ 3, 'Last name must be at least 3 characters long' ],
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [ 5, 'Email must be at least 5 characters long' ],
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    }, socketId: {
+        type: String,
+    }
 })
